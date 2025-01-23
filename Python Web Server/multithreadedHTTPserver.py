a495baf5776f0ea01f7edafc4ec28d1296566e31
elfaidi-ghassen <elfaidighassen@gmail.com>
2025-01-23 17:05:20 +0100

    implemented a thread pool

--- Python Web Server/multithreadedHTTPserver.py
+++ Python Web Server/multithreadedHTTPserver.py
@@ -3,7 +3,38 @@ from datetime import timezone, datetime
 import mimetypes
 import threading
 
-class Log():
+class ThreadPool:
+    def __init__(self, fn, MAX_WORKERS, QUEUE_SIZE):
+        self.MAX_WORKERS = MAX_WORKERS
+        self.QUEUE_SIZE = QUEUE_SIZE
+        self.fn = fn
+        
+        self.full = threading.Semaphore(0)
+        self.empty = threading.Semaphore(QUEUE_SIZE)
+        self.mutex = threading.Semaphore(1)
+        self.tasks_queue = []
+        
+        self.workers = [threading.Thread(target=self.__worker) for _ in range(MAX_WORKERS)]
+        for thread in self.workers:
+            thread.start()
+
+
+    def __worker(self):
+        while True:
+            self.full.acquire()
+            try:
+                with self.mutex:
+                    task = self.tasks_queue.pop()
+                    self.fn(task)
+            finally:
+                self.empty.release()
+    
+    def add_task(self, task):
+        self.empty.acquire()
+        with self.mutex:
+            self.tasks_queue.append(task)
+        self.full.release()
+class Log:
     def __init__(self, filename):
         self.filename = filename
         self.mutex = threading.Lock()
@@ -21,7 +52,7 @@ class Log():
         self.mutex.release()
 
 class HTTPServer:
-    def __init__(self, adress=("", 8080), pending_queue_size=5):
+    def __init__(self, adress=("", 80), pending_queue_size=5):
         self.address = adress
         self.pending_queue_size = pending_queue_size
         self.logs = Log("logs.txt")
@@ -33,12 +64,15 @@ class HTTPServer:
             status_line,
             "Date: " + current_date,
             "Server: Ghassen's HP laptop",
+            "Connection: close",
             "Content-Type: " + mime_type + "",
             "Content-Length: " + str(content_length)
         ]
         return "\r\n".join(headers) + "\r\n\r\n"
     
-    def __process_request(self, connection_socket, client_addr):
+    def __process_request(self, params):
+        connection_socket, client_addr = params
+        
         self.counter_lock.acquire()
         self.active_connections_counter += 1
         print(f"Active Connections: {self.active_connections_counter:03}", end="\r")
@@ -74,11 +108,8 @@ class HTTPServer:
             connection_socket.send(output_headers.encode())
             connection_socket.send(content.encode())
             connection_socket.send("\r\n".encode())
+            connection_socket.close()
             self.logs.add_line(request_line, status_line, current_date, client_addr)
-        except IndexError as e:
-                self.logs.add_error("invalid request", current_date)
-                connection_socket.close()
-
         except FileNotFoundError as e:
 
             with open("404.html") as file:
@@ -109,14 +140,15 @@ class HTTPServer:
         server_socket = socket(AF_INET, SOCK_STREAM)
         server_socket.bind(self.address)
         server_socket.listen(self.pending_queue_size)
-
+        max_pool_workers = 10
+        max_queue_size = 50
+        thread_pool = ThreadPool(self.__process_request, max_pool_workers, max_queue_size)
         print("...the server is ready...")
+        
         while True:
-
             connection_socket, addr = server_socket.accept()
-            request_handler = threading.Thread(target=self.__process_request, args=(connection_socket, addr))
-            request_handler.start()
+            thread_pool.add_task((connection_socket, addr))
 
 
-web_server = HTTPServer(pending_queue_size=5000)
+web_server = HTTPServer(pending_queue_size=5)
 web_server.start()
