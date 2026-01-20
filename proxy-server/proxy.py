ad21d21dbee5a3d1d9b397e2ddaa6f1860dd7437
elfaidi-ghassen <elfaidighassen@gmail.com>
2026-01-20 20:19:41 +0100

    fix: favicon

--- proxy-server/proxy.py
+++ proxy-server/proxy.py
@@ -1,7 +1,16 @@
 import socket
-import ssl
-
 import sys
+
+# forward proxy
+# to test
+# curl.exe http://localhost:8888/www.vulnweb.com/index
+#
+# GET www.vulnweb.com/index HTTP/1.1
+# Host: http://localhost:8888
+#
+# the proxy sends:
+# GET /index HTTP/1.1
+# Host: www.vulnweb.com
 
 TRANSITIONS = {
 	# state        new state     action
@@ -23,7 +32,7 @@ TRANSITIONS = {
 		"DEFAULT": ("READING_RESPONSE_HEADERS", "READ_RESPONSE_HEADERS")
 	},
 	"READING_RESPONSE_BODY": {
-		"DEFAULT": ("INIT", "SEND_RESPONSE_BACK")
+		"DEFAULT": ("DONE", "SEND_RESPONSE_BACK")
 	}
 }
 
@@ -104,10 +113,9 @@ class ProxyServer:
 				self.current_state = "INIT"
 				while True:
 					print(self.current_state)
+					print(server_response_headers)
 					print(current_input)
 					if self.current_state in {"ERROR", "DONE"}:
-						print(request_headers)
-						print(request_body)
 						break
 					if current_input in TRANSITIONS[self.current_state]:
 						self.current_state, action = TRANSITIONS[self.current_state][current_input]
@@ -118,7 +126,13 @@ class ProxyServer:
 
 					if action == "READ_REQUEST_HEADER":
 						reader = HeadersReaderFSM()
-						msg = connection_socket.recv(1024).decode()
+						msg = connection_socket.recv(1024)
+						if not msg:
+							connection_socket.close()
+							break
+
+						msg = msg.decode()
+
 						for char in msg:
 							reader.next(char)
 							if reader.get_current_state() == "ERROR":
@@ -127,6 +141,7 @@ class ProxyServer:
 							if reader.get_current_state() == "DONE":
 								current_input = reader.get_current_state()
 								request_headers = reader.get_headers()
+
 					if action == "READ_REQUEST_BODY":
 						length = self.get_content_length(request_headers)
 						read_bytes = b""
@@ -135,56 +150,83 @@ class ProxyServer:
 						request_body = read_bytes if length else None 
 						current_input = None
 
-					if action == "READ_RESPONSE_HEADERS":
-						to_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-						print(self.get_server_address(request_headers))
-						address = self.get_server_address(request_headers)
-						to_server_socket.connect(address)
+					if action == "SEND_REQUEST":
+						method = request_headers[0].split()[0]
+						if method == "CONNECT":
+							connection_socket.close()
+							print("UNSUPPORTED")
+							break
 
+						# print(request_headers[0])
+						# print(self.get_server_address(request_headers))
+						to_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+						address = self.get_server_address(request_headers)
+						if address[0] == "favicon.ico":
+							connection_socket.close()
+							break
+						to_server_socket.connect(address)
 						new_request_headers = request_headers.copy()
-						full_path = new_request_headers[0].split()[1]  # '/www.example.com/index.html'
+						full_path = new_request_headers[0].split()[1]  # 'http://www.example.com/index.html'
+						full_path = full_path.lstrip('http://')
 						full_path = full_path.lstrip('/')
 						host, _, path = full_path.partition('/')  # host='www.example.com', path='index.html'
 						if path:
 								path = '/' + path
 						else:
 								path = '/'
-
 						method = new_request_headers[0].split()[0]
 						version = new_request_headers[0].split()[2]
 
 						new_request_headers[0] = f"{method} {path} {version}"
 						new_request_headers[1] = f"Host: {host}"
-
+						print(new_request_headers[0])
 						to_server_socket.send(self.to_request_bytes(new_request_headers, request_body))
+
+
+					if action == "READ_RESPONSE_HEADERS":
+						# what.. if... we read more than what we need in the header?
 						reader = HeadersReaderFSM()
-						msg = to_server_socket.recv(1024).decode()
-						for char in msg:
-							reader.next(char)
+						# msg = to_server_socket.recv(1).decode()
+						# print(msg, end="")
+						msg = to_server_socket.recv(1024)
+						for i, char in enumerate(msg):
+							reader.next(chr(char))
 							if reader.get_current_state() == "ERROR":
 								current_input = reader.get_current_state()
 								break
 							if reader.get_current_state() == "DONE":
 								current_input = reader.get_current_state()
 								server_response_headers = reader.get_headers()
+								if i < len(msg) - 1:
+									server_response_body = msg[i + 1:]
+								
 								break
 
 					if action == "READ_RESPONSE_BODY":
 						length = self.get_content_length(server_response_headers)
-						print("length", length)
+						# print("length", length)
+						
 						read_bytes = b""
-						while len(read_bytes) < length:
+						while len(read_bytes) < (length - len(server_response_body or b"")):
 							data = to_server_socket.recv(1024)
-							if data == b"":
-								break
+							# if data == b"":
+							# 	break
 							read_bytes += data
-						server_response_body = read_bytes if length else None 
+						if server_response_body == None:
+							server_response_body = b""
+						server_response_body += read_bytes
+						if not length:
+							 server_response_body = None
 					
 					if action == "SEND_RESPONSE_BACK":
 						response = self.to_response_bytes(server_response_headers, server_response_body)
+						print("yo, it's here!")
 						connection_socket.send(response)
 						connection_socket.close()
 						to_server_socket.close()
+
+
+
 		except KeyboardInterrupt:
 			print("Shutting down server...")
 		finally:
@@ -196,7 +238,8 @@ class ProxyServer:
 		return ("\r\n".join(server_response_headers) + "\r\n\r\n").encode() + (server_response_body or b"")
 	
 	def get_server_address(self, request_headers):
-		full_path = request_headers[0].split()[1].lstrip('/')
+
+		full_path = request_headers[0].split()[1].lstrip('http://')
 		host, _, _ = full_path.partition('/')
 		return (host, 80)
 
